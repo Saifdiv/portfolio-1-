a000d6fa32253196b3fb2a963c1756d658e894e3
Saifdiv <seif.islam2520@gmail.com>
2025-03-16 17:41:03 +0100

    Add files via upload

---
+++ tass.js
@@ -0,0 +1,9 @@
+const op = document.getElementById("ba")
+op.addEventListener("click",function(){
+    document.getElementById("list").classList.add("show")
+})
+const po = document.getElementById("bb")
+po.addEventListener("click",function(){
+    document.getElementById("list").classList.remove("show")
+})
+
